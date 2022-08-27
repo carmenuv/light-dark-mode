2c6876eaaf7e1ec0bcdf59c23c4c8b3d2b62180b
carmenuv <carmenurizarv@gmail.com>
2022-08-27 17:58:27 -0500

    scss y js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-
+const btnswitch = document.querySelector("#switch");
+btnswitch.addEventListener("click", ()=>{
+  document.body.classList.toggle("dark");
+  btnswitch.classList.toggle("active");
+});
